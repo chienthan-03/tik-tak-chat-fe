394a7ce0ac101a8730ac9e558c2f873dbb792154
Daniel Khuc <luan.khucthanh@euroland.com>
2025-05-03 16:53:07 +0700

    fix: theme icon

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,22 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import ChatProvider from "./Context/ChatProvider.jsx";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+// Create a default MUI theme
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
-    <BrowserRouter>
-      <ChatProvider>
-        <App />
-      </ChatProvider>
-    </BrowserRouter>
-  </ChakraProvider>
+  <ThemeProvider theme={theme}>
+      <ChakraProvider>
+      <BrowserRouter>
+        <ChatProvider>
+          <App />
+        </ChatProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </ThemeProvider>
 );
